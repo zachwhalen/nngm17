807aa1fd91f75ea8c576b7c170ef3c7a2d21d3af
Zach Whalen <zach.whalen@gmail.com>
2017-12-11 14:38:17 -0500

    Almost there I think

--- house-that-conceptnet-built.py
+++ house-that-conceptnet-built.py
@@ -4,7 +4,12 @@ import requests
 import random
 import re
 import os
-
+import json
+import glob
+import urllib
+from requests_oauthlib import OAuth1
+import flickrapi
+import pycorpora
 from textblob import TextBlob 
 from textblob.tokenizers import SentenceTokenizer
 
@@ -13,7 +18,48 @@ from pattern.en import tenses, PAST, PL, parse, pluralize, singularize
 
 import pdfkit
 
+from PIL import Image
 concept_cache = {}
+def pal():
+    palettes = [
+        ["cf3e27","0d8a89","066598","ff9800","e68900"],
+        ["25CAF7","BEF272","F8FA68","F89573","F44750"],
+        ["EA9155","EFB53A","C54400","B0946F","5D443F"],
+        ["820434","C84622","F6871E","F3B778","2D132E"],
+        ["1E2C45","A1531B","D9B32B","A11B1B","250433"]
+    ]
+    
+    return random.choice(palettes)
+
+def cred():
+    cred = {}
+    f = open("credentials")
+    lines = f.readlines()
+    for l in lines:
+        l = l.rstrip()
+        cred[l.split(" = ")[0]] = l.split(" = ")[1] 
+    return cred
+
+def tpl(template_file,outfile,data):
+    # data should be a list of tuples
+    
+    if os.path.isfile(outfile):
+        os.unlink(outfile)
+    f = open(template_file)
+    lines = f.readlines()
+    f.close()
+    
+    for l in lines: 
+        for token in data: 
+#             print "token0 is " + token[0]
+#             print "token1 is " + token[1]
+            l = l.replace("<!-- " + token[0] + " -->",token[1])
+        g = open(outfile,"a")
+
+        g.write(l)
+        f.close()
+   
+    return True
 
 def simpler(word):
     return re.sub(r"^an? |^the |^your ","",word,flags=re.IGNORECASE)
@@ -30,6 +76,8 @@ def pastify(phrase):
                 fixed += "left "
             elif ("lose" in w[0]):
                 fixed += "lost "
+            elif ("lie" in w[0]):
+                fixed += "lay "
             else:
                 fixed += conjugate(w[0],tense=PAST) + " "
             found = 1
@@ -88,6 +136,7 @@ def get_some(word,rel,direction,number):
 def stack(seed,depth):
     db = []
     completed = []
+    og = seed
     
     for d in range(depth):
         
@@ -187,90 +236,136 @@ def stack(seed,depth):
         db = [v for i, v in enumerate(db) if i not in trim]
         
         print "After cleanup, db is at " + str(len(db))
-            
-    return completed
-
-def prepare():
-    ordered = list(result[-1][::-1])
-    chapter = ''
-    ordered.append("the tiger")
-    colors = ["#cf3e27","#0d8a89","#066598","#ff9800","#e68900"]
-    for page in range(len(ordered)):
-        
-        template = open('template.html')
-        lines = template.readlines()
-        template.close()
-        
-        page_number = str(page + 1)
-        
-        the_concept = pastify(specify(ordered[page]))
-        
-        # isolate the current object
-        the_thing = the_concept.split(" the ")[-1]
-        
-        
-  
-        if (page == 0):
-            next_concept = ""
-            chapter = "that Jack built."
-        else:
-            color = random.choice(colors)
-            next_concept = "<span style=\"color:" + color + "\"> that " + pastify(specify(ordered[page - 1])) + "</span>"
-        
-        #print "\n\nThis is the " + the_thing + next_concept
-        #print chapter
-        
-        gen = open( "pages/" + page_number + ".html", 'w' )
-        for l in lines:
-            li = l.replace("{{{pn}}}",page_number).replace("{{{chapter_content}}}",chapter).replace("{{{first_line}}}","This is the <span class='page-object'>" + the_thing + "</span>" + next_concept)
-            gen.write(li)
-            
-        gen.close()
-        
-        chapter = next_concept + chapter
-        
-def get_icons(keyword):
     
+    # pick one of the longest complete chains, prepend the seed, and return 
+    content = completed[-1]
+    content.insert(0,str(og))
+    return content
+
+def get_icons(noun,fallback):
+    save_as = ""
+    keyword = re.sub(r" $","",noun)
     # first check the cache
-    if (os.path.isfile("icons/" + keyword + ".json")):
-        #print "Loading from file"
+    if (os.path.isfile("icons/" + keyword.replace(" ","+") + ".json")):
+        print "Loading from file"
         # proceed to load it from file
         
-        icon_file = open("icons/" + keyword + ".json").read()
+        icon_file = open("icons/" + keyword.replace(" ","+") + ".json").read()
         icons = json.loads(icon_file)
         #icon_file.close()
-        
+        save_as = keyword
+    
+    elif (os.path.isfile("icons/" + keyword.split(" ")[-1] + ".json")):
+        icon_file = open("icons/" + keyword.split(" ")[-1] + ".json").read()
+        icons = json.loads(icon_file)
+        save_as = keyword.split(" ")[-1]
     else:
-        #print "Getting from API"
+        print "Getting from API"
         # set up nounproject auth
-        auth = OAuth1("da866a7951e24154b918294bd9223304", "072eca50d5dc47d3a98375e6ef54a434")
-
+        credentials = cred()
+        auth = OAuth1(credentials['noun_key'],credentials['noun_secret'])
+ 
         # the api endpoint
-        endpoint = "http://api.thenounproject.com/icons/{}".format(keyword)
-
+        endpoint = "http://api.thenounproject.com/icons/" + urllib.quote(keyword)
+        
+       
         response = requests.get(endpoint, auth=auth)
-        
-        icons = response.json()
-        
-        with open("icons/" + keyword + ".json","w") as outfile:
-            json.dump(data, outfile)
-        
-    return icons        
+        print response.status_code
+        if ("404" not in str(response.status_code)):
+            icons = response.json()
+            save_as = keyword
+        elif (len(keyword.split(" ")) > 1):
+            # try again
+            # should put a file check in here
+            endpoint = "http://api.thenounproject.com/icons/" + urllib.quote(keyword.split(" ")[-1])
+            response = requests.get(endpoint, auth=auth)
+            
+            if ("404" not in str(response.status_code)):
+                icons = response.json()
+                save_as = keyword.split(" ")[-1]
+            else:
+                icons = json.loads("icons/object.json")
+                save_as = "object"
+                
+        else:
+#             icons = json.loads("icons/thing.json")
+#             save_as = "thing"
+            icons = get_icons(fallback,fallback)
+            save_as = fallback
+            
+        with open("icons/" + save_as.replace(" ","+") + ".json","w") as outfile:
+            json.dump(icons, outfile)
+            
+    return icons
+     
 
-def get_flickr_image(keyword):
+def get_flickr_image(keyword,chapter_number):
     
-    api_key = u'7cc80add2cd96da6fca533e9eeb815b4'
-    api_secret = u'79826baa9f006ad3'
+    new_fn = 'images/' + keyword + '-' + chapter_number + '-watercolor.jpg'
+    if (not os.path.isfile(new_fn)):
+    
+        credentials = cred()
+        api_key = credentials['flickr_key']
+        api_secret = credentials['flickr_secret']
 
-    flickr = flickrapi.FlickrAPI(api_key, api_secret,format="parsed-json")
-    photos = flickr.photos.search(text=keyword,license='4,5,9,10',sort="relevance",per_page='40')
-    photo = random.choice(photos['photos']['photo'])
-    fullsize = flickr.photos.getSizes(photo_id=photo['id'])['sizes']['size'][-1]['source']
-    fn = fullsize.split("/")[-1]
-    iid = fn.split(".")[0]
-    new_fn = fn.split(".")[0] + "-watercolor.jpg"
-    
-    os.system("wget -P images " + fullsize)
-    
-    os.system("./watercolor -s 25 -e 5 -m 50 -c 0 images/" + fn + " images/" + new_fn)
+        flickr = flickrapi.FlickrAPI(api_key, api_secret,format="parsed-json")
+        photos = flickr.photos.search(text=keyword,license='4,5,9,10',sort="relevance",per_page='300')
+        photo = random.choice(photos['photos']['photo'])
+        fullsize = flickr.photos.getSizes(photo_id=photo['id'])['sizes']['size'][-1]['source']
+        fn = fullsize.split("/")[-1]
+        iid = fn.split(".")[0]
+        #new_fn = fn.split(".")[0] + "-watercolor.jpg"
+
+        os.system("wget -P images " + fullsize)
+        # convert images/larger-flickr.jpg -resize "800x800^" -gravity center -crop 800x800+0+0 +repage -brightness-contrast 50x0 images/smaller-flickr.jpg
+
+        os.system("convert images/" + fn + " -resize \"800x800^\" -gravity center -crop 800x800+0+0 +repage -brightness-contrast 50x0 images/" + fn)
+        os.system("./watercolor -s 25 -e 5 -m 50 -c 0 images/" + fn + " " + new_fn)
     return new_fn
+
+def get_icon(keyword,color,fallback="thing"):
+    icons = get_icons(keyword,fallback)
+    #print icons
+    
+    icon_url = 0
+    while(not icon_url):
+        icon = random.choice(icons['icons'])
+        
+        if ("attribution_preview_url" in icon):
+            print "has png"
+            icon_url = icon['attribution_preview_url']
+            
+            # do I already have this one?
+            if (not os.path.isfile("images/" + icon['id'] + ".png")):
+                # save it
+                get = os.system("wget -P images/ -O images/" + icon['id'] + ".png " + icon_url)
+
+                # crop it
+                img = Image.open("images/" + icon['id'] + ".png")
+                t = img.crop((0,0,700,600))
+                t.save("images/" + icon['id'] + ".png")
+            
+            # do I have the right color?
+            color_fn = "images/" + icon['id'] + "-" + color + ".png"
+            print "The colorized file name should be " + color_fn
+            if (not os.path.isfile(color_fn)):
+                # convert 8668.png -fuzz 100% -fill "#e68900" -opaque black test.png
+                print "coloring the icon "
+                os.system("convert images/" + icon['id'] + ".png " + "-fuzz 100% -fill \"#" + color + "\" -opaque black " + color_fn)
+    
+    top = "%.1f" % random.uniform(0,7.0)
+    left = "%.1f" % random.uniform(0.5,7.0)
+    width = "%.1f" % random.uniform(0.2,6.3)
+    rotation = "%.1fdeg" % random.uniform(-30,30)
+    
+    position = "top:" + top + "in;" + "left:" + left + "in;"
+    
+    return u'<img src=\"../' + color_fn + '\" class=\"' + keyword + '\" style=\"transform:rotate(' + rotation + ');position:absolute;width:' + width + 'in;' + position + '\" />'
+
+def a(phrase):
+    if (re.search(r" the [aeiou]",phrase)):
+        new_phrase = phrase.replace(" the ", " an ")
+    else:
+        new_phrase = phrase.replace(" the ", " a ")
+    return new_phrase
+    
